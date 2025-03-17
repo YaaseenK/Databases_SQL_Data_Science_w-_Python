093ffa0a92ec6cdc95958508c8e468cf80a2511b
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-17 17:55:07 -0400

    added 3 questions for LIMIT and formating in answers doc

--- module1/PracticeQuestions.sql
+++ module1/PracticeQuestions.sql
@@ -9,3 +9,9 @@
 -- 1. Retrieve the names of all unique films released in the 21st century and onwards, along with their release years.
 -- 2. Retrieve the directors' names and their distinct films shot at City Hall.
 -- 3. Retrieve the number of distributors who distributed films with the 1st actor, Clint Eastwood.
+
+-- LIMIT
+-- 1. Retrieve the names of the first 50 films.
+-- 2. Retrieve the first 10 film names released in 2015.
+-- 3. Retrieve the next 3 film names that follow after the first 5 films released in 2015.
+
