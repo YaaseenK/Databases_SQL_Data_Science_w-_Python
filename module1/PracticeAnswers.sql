b9cd0fc4a6782307cb67f3298deec42643bcd00b
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-17 17:52:46 -0400

    answered 3 questions using the distict keyword

--- module1/PracticeAnswers.sql
+++ module1/PracticeAnswers.sql
@@ -9,5 +9,8 @@ SELECT COUNT("Release Year") FROM FilmLocations WHERE "Release Year" < 1950;
 
 -- DISTICT
 -- 1.
+SELECT DISTINCT Title, "Release Year" FROM FilmLocations WHERE "Release Year" > 2000 
 -- 2.
+SELECT DISTINCT Director, TITLE FROM FilmLocations Where Locations = "City Hall"
 -- 3.
+SELECT COUNT (Distributor) from FilmLocations where "Actor 1" = "Clint Eastwood" 
