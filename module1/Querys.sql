ac62c55886c8fa02921a312f42787b6ba765d6f1
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-17 16:39:40 -0400

    added some queries and commented its functionality

--- module1/Querys.sql
+++ module1/Querys.sql
@@ -1,2 +1,9 @@
--- Selects everything from FilmLocation table but prints the first 5
+-- Selects and displays the first 5 rows from the FilmLocations table
 SELECT * FROM FilmLocations LIMIT 5;
+
+-- Returns the total number of records (rows) in the FilmLocations table
+SELECT COUNT(*) FROM FilmLocations;
+
+-- Counts how many film locations have a writer named "James Cameron"
+-- This counts only the rows where the Writer column equals "James Cameron"
+SELECT COUNT(Locations) FROM FilmLocations WHERE Writer = "James Cameron";
