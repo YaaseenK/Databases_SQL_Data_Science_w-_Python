eeaa5a54cfe95431b46e8a45d28fd2723f06e409
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-17 17:33:49 -0400

    answered the first 3 questions

--- module1/PracticeAnswers.sql
+++ module1/PracticeAnswers.sql
@@ -1,2 +1,8 @@
 -- SQLite
-SELECT COUNT(Locations) FROM FilmLocations WHERE Director = "Woody Allen"
+SELECT * FROM Filmlocations LIMIT 5;
+-- 1.
+SELECT COUNT(Locations) FROM FilmLocations WHERE Director = "Woody Allen";
+-- 2.
+SELECT COUNT (TITLE) FROM FilmLocations Where Locations = "Russian Hill";
+-- 3.
+SELECT COUNT("Release Year") FROM FilmLocations WHERE "Release Year" < 1950;
