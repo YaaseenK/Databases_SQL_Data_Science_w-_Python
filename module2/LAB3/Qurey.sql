45b9bac06075cc186918491ed3eed9836ccc3efc
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-19 19:43:09 -0400

    answered q 2

--- module2/LAB3/Qurey.sql
+++ module2/LAB3/Qurey.sql
@@ -63,9 +63,11 @@ ORDER BY AVG_SALARY;
 -- 1. Retrieve the list of all employees, first and last names, whose first names start with ‘S’.
     SELECT F_NAME, L_NAME
     FROM EMPLOYEES
-    WHERE F_NAME in 'S%'
--- 2. Arrange all the rec ords of the EMPLOYEES table in ascending order of the date of birth.
+    WHERE F_NAME LIKE 'S%'
 
+-- 2. Arrange all the records of the EMPLOYEES table in ascending order of the date of birth.
+SELECT * from EMPLOYEES 
+ORDER BY B_DATE;
 -- 3. Group the records in terms of the department IDs and filter them of ones that have average salary more than or equal to 60000. Display the department ID and the average salary.
 
 -- 4. For the problem above, sort the results for each group in descending order of average salary. 
