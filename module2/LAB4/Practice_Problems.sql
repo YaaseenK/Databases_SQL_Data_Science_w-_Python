ed89c5d9264a4e2f4dbaa60f5696c20eba01e7d2
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-20 23:25:34 -0400

    q2

--- module2/LAB4/Practice_Problems.sql
+++ module2/LAB4/Practice_Problems.sql
@@ -2,8 +2,13 @@
 
 -- 1. Write a query that displays the average cost of rescuing a single dog.
 -- Note that the cost per dog would not be the same in different instances.
+SELECT * from Petrescue
+Select AVG(COST/QUANTITY) as "AVG_DOG_COST"
+from PETRESCUE 
+where ANIMAL = "Dog";
 
 -- 2. Write a query that displays the animal name in each rescue in uppercase without duplications.
+SELECT upper(ANIMAL) from PETRESCUE;
 
 -- 3. Write a query that displays all the columns from the PETRESCUE table where the animal(s) rescued are cats.
 -- Use 'cat' in lowercase in the query.
