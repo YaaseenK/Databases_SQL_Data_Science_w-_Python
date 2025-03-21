cfc8395a2250ce634315468fe72fc3c5ab86e015
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-20 23:19:17 -0400

    added practice problem questions

---
+++ module2/LAB4/Practice_Problems.sql
@@ -0,0 +1,14 @@
+-- Practice Problems
+
+-- 1. Write a query that displays the average cost of rescuing a single dog.
+-- Note that the cost per dog would not be the same in different instances.
+
+-- 2. Write a query that displays the animal name in each rescue in uppercase without duplications.
+
+-- 3. Write a query that displays all the columns from the PETRESCUE table where the animal(s) rescued are cats.
+-- Use 'cat' in lowercase in the query.
+
+-- 4. Write a query that displays the number of rescues in the 5th month.
+
+-- 5. The rescue shelter is supposed to find good homes for all animals within 1 year of their rescue.
+-- Write a query that displays the ID and the target date.
