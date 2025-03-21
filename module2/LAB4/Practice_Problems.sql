9d7606a371c5d1fc9ea2fd6c502f9c223717fd5b
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-21 10:09:15 -0400

    lab 5 notes

--- module2/LAB4/Practice_Problems.sql
+++ module2/LAB4/Practice_Problems.sql
@@ -8,11 +8,11 @@ from PETRESCUE
 where ANIMAL = "Dog";
 
 -- 2. Write a query that displays the animal name in each rescue in uppercase without duplications.
-SELECT upper(ANIMAL) from PETRESCUE;
+SELECT DISTINCT upper(ANIMAL) from PETRESCUE;
 
 -- 3. Write a query that displays all the columns from the PETRESCUE table where the animal(s) rescued are cats.
 -- Use 'cat' in lowercase in the query.
-
+SELECT * FROM PETRESCUE where lower(ANIMAL) = 'Cat'; 
 -- 4. Write a query that displays the number of rescues in the 5th month.
 
 -- 5. The rescue shelter is supposed to find good homes for all animals within 1 year of their rescue.
