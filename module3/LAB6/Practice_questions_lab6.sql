72581aec40d6b38ba77bbefd9a3f19b42c6cdf8b
Yaaseen <yaaseenkhan02@gmail.com>
2025-03-23 21:32:17 -0400

    started module 4

--- module3/LAB6/Practice_questions_lab6.sql
+++ module3/LAB6/Practice_questions_lab6.sql
@@ -5,14 +5,22 @@
 
 -- Solution:
 
+SELECT * FROM Employees 
+WHERE JOB_ID IN (SELECT JOB_IDENT FROM JOBS WHERE JOB_TITLE = 'Jr. Designer');
+
+
 -- b. Using Implicit Joins
 
 -- Solution:
+SELECT E.*
+FROM EMPLOYEES E 
+Join JOBS J ON E.JOB_ID = JOB_IDENT;
 
 -- 2. Retrieve JOB information and a list of employees whose birth year is after 1976.
 -- a. Using sub-queries
 
 -- Solution:
+
 
 -- b. Using Implicit Joins
 
